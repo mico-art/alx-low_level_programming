954dc33032b5f4cca43fe6d5218f4b7c025133f2
mico-art <micoyiftu@gmail.com>
2022-03-14 13:54:56 +0300

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,9 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
+	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	return 1;
 }
+
