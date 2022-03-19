59332091c772d69fa00cae33fdb9a943b8f529df
mico-art <micoyiftu@gmail.com>
2022-03-20 00:01:19 +0300

    rm

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,3 @@
-#include "holberton.h"
 #include "main.h"
 #include <stdio.h>
 /**
@@ -6,7 +5,7 @@
  * @c: input
  * Return: 1 if c is uppercase, 0 otherwise
  */
-int _isupper(int c);
+int _isupper(int c)
 {
 	if ((c >= 'A') && (c <= 'Z'))
 		return (1);
