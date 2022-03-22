6b6566f862be6afb78c077608e290d60365ae548
mico-art <micoyiftu@gmail.com>
2022-03-22 19:19:15 +0300

    pointer array

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>   //not required    
+#include <time.h>
+#include "main.h"
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+int main(void) //int main() is not a valid signature in C
 {
+    srand((unsigned int)(time(NULL)));
+    int i;
+    char pass[13]; //extra byte for null terminating character
 
-	return (0);
+    printf("Press enter to get a twelve-character password\n");
+    getchar();
+
+    for (i = 0; i < 4; i++) 
+{
+        //revised logic to generate random characters at all positions (0 - 11)
+        pass[ 3 * i ] = '0' + (rand() % 10); //generating numeric character
+        char capLetter = 'A' + (rand() % 26); //generating upper case alpha character
+        pass[(3 * i) + 1] = capLetter;
+        char letter = 'a' + (rand() % 26); //generating lower case alpha character
+        pass[(3 * i) + 2] = letter;
 }
+    pass[3 * i] = '\0'; //placing null terminating character at the end
+    printf("generated password : %s\n\n",pass); //printing the string
+
+    printf("\n\n");
+}
+
 
