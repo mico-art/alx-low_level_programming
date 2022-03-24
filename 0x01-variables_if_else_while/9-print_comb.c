6dcb222f2e241ed77beab3e56373666d224262cb
mico-art <micoyiftu@gmail.com>
2022-03-24 19:30:36 +0300

    upadte

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,16 +13,14 @@ int main(void)
         for (d = '0'; d <= '9'; d++)
 {
                 putchar(d);
-}
+
                 if (d != '9')
-{
+
                         putchar(',');
-}
 
                         putchar(' ');
-
+}
         putchar('\n');
 
         return (0);
 }
-
